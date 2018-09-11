684ea1fc01f782ec797334c95a179bcf23dfbe75
Kaelem Moniz <kaelem.moniz@9828.local>
2018-09-11 15:14:35 -0400

    First Commit

---
+++ HelloWorld.py
@@ -0,0 +1,3 @@
+print("Yo Man. Theres a place you should know!")
+print("I say yo man! Theres a place you should go")
+print("you can stay there! at the YMCA but you've got. to . know this one thing")
