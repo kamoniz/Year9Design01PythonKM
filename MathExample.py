90b7423973ee7e23556a6279244d24b4da8f7526
Kaelem Moniz <kaelem.moniz@9828.local>
2018-09-17 13:54:01 -0400

    MathExample.py

--- MathExample.py
+++ MathExample.py
@@ -1,2 +1,11 @@
 x=1+2
 print (x)
+#These are comments in Jave. When you program 
+#and I look at your work I always check your comments -Mr. Miskew
+x=1+2 #this creates a variable called x and puts 1 + 2 in it
+#An assignment statment is any situation when I have something 
+#equals something
+x = x + 1
+print (x)
+x= x +5
+print (x)
