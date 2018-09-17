c4d27f2bfc5d1d603948ee0c6485b93cb776d579
Kaelem Moniz <kaelem.moniz@9828.local>
2018-09-17 13:34:01 -0400

    math example

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,4 @@
 print("Yo Man. Theres a place you should know!")
 print("I say yo man! Theres a place you should go")
 print("you can stay there! at the YMCA but you've got. to . know this one thing")
+print("Hello World")
