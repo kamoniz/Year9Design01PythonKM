ee5ab4d92372e57f73a22767430356c75af89ae6
Kaelem Moniz <kaelem.moniz@10385.local>
2018-11-01 11:55:40 -0400

    DIStringDemo

---
+++ DI StringDemo01.py
@@ -0,0 +1,4 @@
+fname = input("Input First Name")
+lname = input("Input Last Name")
+
+userName = fName[0] + lName
---
+++ DIStringDemo01.py
@@ -0,0 +1,6 @@
+fname = input("Input First Name")
+lname = input("Input Last Name")
+
+userName = fname[0] + lname
+
+return "Hello fname + lname"
